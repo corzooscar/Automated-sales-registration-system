17bfad1d4f430ae86d9d6a30ba1f6cf73043b99b
Oczo <corzooscar143@gmail.com>
2026-03-11 07:24:06 -0500

    Commit: Small checkpoint

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
-from Funciones_Reg_Cal import registrar_venta
+from Funciones_Reg_Cal_Tot import registrar_venta
+
 Lista_ventas = []
 
-Deseo = input("Desea registrar una venta? s/n: ").lower()
+print(f"""╔═══════════════════════════════════════════════════════════╗
+                🏦 SALES REGISTRATION SYSTEM 🏦
+╚═══════════════════════════════════════════════════════════╝""")
+Deseo = input("~ Desea registrar una venta? s/n: ").lower()
 while Deseo == "s": 
     venta = registrar_venta()
     Lista_ventas.append(venta)
-    print(f"""{"="*60}
-Producto: {Lista_ventas[-1]['Producto']} 
-Precio: {Lista_ventas[-1]['Precio']}
-Cantidad: {Lista_ventas[-1]['Cantidad']} 
-SubTotal: {Lista_ventas[-1]['Total']}
+    print(f"""{"-"*60} 
+ ~ SubTotal: ${Lista_ventas[-1]['Total']} | Producto: {Lista_ventas[-1]['Producto']} 
+{"="*60}
 """)
-    Deseo = input("Desea registrar otra venta? s/n: ").lower()
+    Deseo = input("~ Desea registrar otra venta? s/n: ").lower()
 
-print("\n")
-print("-"*60)
-print("RESUMEN DE VENTAS DEL DÍA")
-print("-"*60)
+print(f"""{"="*60}
+{"RESUMEN DE VENTAS DEL DÍA".center(60) }
+{"="*60}""")
 for venta in Lista_ventas:
-    print(f"""El total a pagar por {venta['Cantidad']} {venta['Producto']}(s) es: ${venta['Total']:.2f}
-""")
-print
-print("Gracias por utilizar el programa de registro de ventas.")
+    print(f"""— Producto: {venta['Producto']} 
+  Precio U: ${venta['Precio']:.2f}        
+  Cantidad: {venta['Cantidad']}  
+  Subtotal: ${venta['Total']:.2f}
+  -------------------------------""")
+print(f"— Total Recaudado: ${sum(venta['Total'] for venta in Lista_ventas):.2f}")
+print("="*60)
+print("~ Gracias por utilizar el programa de registro de ventas ʕ•́ᴥ•̀ʔっ")
