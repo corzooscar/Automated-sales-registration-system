1b755bd917e28e479b4edd1805b8a4d074ac714b
Oczo <corzooscar143@gmail.com>
2026-03-11 08:26:47 -0500

    Commit: Final version of the code in spanish

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Funciones_Reg_Cal_Tot import registrar_venta
+from Funciones_Reg_Cal_Tot import registrar_venta, mostrar_total, mostrar_resumen
 
 Lista_ventas = []
 
@@ -10,20 +10,12 @@ while Deseo == "s":
     venta = registrar_venta()
     Lista_ventas.append(venta)
     print(f"""{"-"*60} 
- ~ SubTotal: ${Lista_ventas[-1]['Total']} | Producto: {Lista_ventas[-1]['Producto']} 
+~ SubTotal: ${Lista_ventas[-1]['Total']} | Producto: {Lista_ventas[-1]['Producto']} 
 {"="*60}
 """)
     Deseo = input("~ Desea registrar otra venta? s/n: ").lower()
-
-print(f"""{"="*60}
-{"RESUMEN DE VENTAS DEL DÍA".center(60) }
-{"="*60}""")
-for venta in Lista_ventas:
-    print(f"""— Producto: {venta['Producto']} 
-  Precio U: ${venta['Precio']:.2f}        
-  Cantidad: {venta['Cantidad']}  
-  Subtotal: ${venta['Total']:.2f}
-  -------------------------------""")
-print(f"— Total Recaudado: ${sum(venta['Total'] for venta in Lista_ventas):.2f}")
+  
+mostrar_resumen(Lista_ventas)
+print(mostrar_total(Lista_ventas))
 print("="*60)
 print("~ Gracias por utilizar el programa de registro de ventas ʕ•́ᴥ•̀ʔっ")
